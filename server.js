d54c53ca47786c630f715b345c0b65eddd9bdca6
percivalho <35653644+percivalho@users.noreply.github.com>
2023-06-16 12:42:47 +0100

    support uuid and delete function

--- server.js
+++ server.js
@@ -49,22 +49,21 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-// POST request to add a review
-app.post('/api/reviews', (req, res) => {
+// POST request to add a new Note
+app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a review`);
 
   // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
+  const { title, text } = req.body;
 
   // If all the required properties are present
-  if (product && review && username) {
+  if (title && text) {
     // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
     };
 
 
@@ -72,46 +71,88 @@ app.post('/api/reviews', (req, res) => {
     //const reviewString = JSON.stringify(newReview);
 
     // Obtain the existing reviews:
-    fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        let reviews = JSON.parse(data);
+        let notes = JSON.parse(data);
 
-        console.log(reviews);
+        console.log(notes);
 
-        reviews.push(newReview); 
+        notes.push(newNote); 
 
-        fs.writeFile(`./db/reviews.json`, JSON.stringify(reviews), (err) =>
+        fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, 2), (err) =>
         err
           ? console.error(err)
           : console.log(
-              `Review for ${newReview.product} has been added to JSON file`
+              `Note for ${newNote.title} has been added to JSON file`
             )
       );        
       }
     })
 
-    // Write the string to a file
-    /*fs.writeFile(`./db/reviews.json`, reviewString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newReview.product} has been written to JSON file`
-          )
-    );*/
-
     const response = {
       status: 'success',
-      body: newReview,
+      body: newNote,
     };
 
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json('Error in posting review');
+    res.status(500).json('Error in posting note');
   }
 });
+
+// DELETE request to delete a Note
+app.delete('/api/notes/:id', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  const id = req.params.id;
+  console.log(id);
+
+  // make sure id is found to delete
+  if (id) {
+
+    // Obtain the existing reviews:
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let notes = JSON.parse(data);
+
+        console.log(notes);
+        let deleteNoteTitle = ""; 
+        //find the corresponding note:
+        let index = notes.findIndex(item => item.id === id);        
+        // if the object was found in the array, remove it
+        if (index !== -1) {
+          deleteNoteTitle = notes[index].title;
+          notes.splice(index, 1);
+        }
+
+        fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, 2), (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `Note for ${deleteNoteTitle} with id ${id} has been deleted`
+            )
+      );        
+      }
+    })
+
+    const response = {
+      status: 'success',
+      body: currentNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in deleting note');
+  }
+});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
