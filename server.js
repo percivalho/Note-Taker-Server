698be88146f8b0dfc949b45be115b4672aab03df
percivalho <35653644+percivalho@users.noreply.github.com>
2023-06-19 20:05:48 +0100

    add params in port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
